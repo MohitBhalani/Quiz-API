7219459a8d8d3a09617cde339b4e11161eec6cf9
MohitBhalani <bhalanimohit123@gmail.com>
2025-02-06 14:35:52 +0530

    use cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var cors = require('cors');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
@@ -22,6 +23,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/Quiz')
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
